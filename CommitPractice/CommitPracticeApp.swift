1121a3b0a7d96fe37cc29c3814fbb102d27307ae
Jacob Kappler <jacobkappler@gmail.com>
2026-02-26 14:01:31 -0500

    Initial Commit

---
+++ CommitPractice/CommitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitPracticeApp.swift
+//  CommitPractice
+//
+//  Created by Jacob Kappler on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CommitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
